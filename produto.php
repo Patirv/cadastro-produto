fdc1b69e90c92c36088f494ed5481c39df867b11
Patirv <patricia.valenzuel@estudante.ifms.edu.br>
2022-07-26 22:04:25 -0400

    tres

---
+++ produto.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=, initial-scale=1.0">
+  <link rel="stylesheet" href ="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <title>Document</title>
+</head>
+<body>
+<div class="jumbotron" style="text-align:center;background-color:#c9ebed" >
+
+    <h1>Cadastre seu Produto</h1>
+</div>
+  <form action="">
+    
+  <div class="form-group">
+                <label for="tipo">Tipo:</label>
+                <input type="text" class="form-control" id="tipo" placeholder="Digiti o tipo do produto" name="nome">
+
+            </div>
+                <div class="form-group">
+                <label for="nome">Data de Fabricação</label>
+                <input type="text" class="form-control" id="data" placeholder="Digite a data de fabricação" name="nome">
+
+            </div>       
+
+            <div class="form-group">
+                <label for="nome">Data de Vencimento</label>
+                <input type="text" class="form-control" id="nome" placeholder="Digite a data de vencimento" name="nome">
+
+            </div>
+            <div class="form-group">
+                <label for="nome">Preço da Unidade</label>
+                <input type="text" class="form-control" id="nome" placeholder="Digite o preço da unidade" name="nome">
+
+            </div>
+            <div class="form-group">
+                <label for="nome">Quantidade</label>
+                <input type="text" class="form-control" id="nome" placeholder="Digite a quantidade de produtos" name="nome">
+            </div>
+            <button type="submit" class="btn btn-default">Enviar</button>
+
+            <div class="jumbotron" style="text-align:center;background-color:#c9ebed" ></div>
+  
+  </form>
+</body>
+</html>
